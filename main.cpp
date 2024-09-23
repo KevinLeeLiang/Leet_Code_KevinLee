88ad4ed979cfb6ab4cf38c23c82a4168cd99d168
garen_lee <lijialiang@haomo.ai>
2024-09-23 12:32:22 +0800

    更新729,730题

--- main.cpp
+++ main.cpp
@@ -6427,10 +6427,79 @@ void selfDividingNumbers_test(){
     print_vector(ans);
 }
 
+namespace MyCalendar {
+    class MyCalendar {
+        set<pair<int, int>> booked;
+
+    public:
+        bool book(int start, int end) {
+            auto it = booked.lower_bound({end, 0});
+            if (it == booked.begin() || (--it)->second <= start) {
+                booked.emplace(start, end);
+                return true;
+            }
+            return false;
+        }
+    };
+}
+
+void MyCalendar_test(){
+    MyCalendar::MyCalendar test;
+    cout << test.book(10, 20) << endl;
+    cout << test.book(15, 25) << endl;
+    cout << test.book(20, 30) << endl;
+    cout << "+++++++++" << endl;
+}
+
+namespace countPalindromicSubsequences {
+    const int MOD = 1E9+7;
+    int countPalindromicSubsequences(string s) {
+        int n = s.size();
+        vector<vector<vector<int>>> dp(4, vector<vector<int>>(n, vector<int>(n, 0)));
+        for (int i = 0; i < n; i++) {
+            dp[s[i] - 'a'][i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0, j = len - 1; j < n; i++, j++) {
+                for (char c = 'a', k = 0; c <= 'd'; c++, k++) {
+                    if (s[i] == c && s[j] == c) {
+                        dp[k][i][j] = (2LL + dp[0][i + 1][j - 1] + dp[1][i + 1][j - 1] + dp[2][i + 1][j - 1] + dp[3][i + 1][j - 1]) % MOD;
+                    } else if (s[i] == c) {
+                        dp[k][i][j] = dp[k][i][j - 1];
+                    } else if (s[j] == c) {
+                        dp[k][i][j] = dp[k][i + 1][j];
+                    } else {
+                        dp[k][i][j] = dp[k][i + 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < 4; i++) {
+            res = (res + dp[i][0][n - 1]) % MOD;
+        }
+        return res;
+    }
+}
+
+void countPalindromicSubsequences_test(){
+    string s;
+    s = "bccb";
+    cout << countPalindromicSubsequences::countPalindromicSubsequences(s) << endl;
+    s = "abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba";
+    cout << countPalindromicSubsequences::countPalindromicSubsequences(s) << endl;
+}
+
 int main() {
-    selfDividingNumbers_test();
+    countPalindromicSubsequences_test();
     {
-    //countOfAtoms_test();
+    //MyCalendar_test();
+
+        //selfDividingNumbers_test();
+
+        //countOfAtoms_test();
 
         //splitListToParts_test();
 
