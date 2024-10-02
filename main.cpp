ccac34dd63a072abb04bca9b4cf409e5e34b5d04
garen-lee <srljl@163.com>
2024-10-02 11:08:06 +0800

    更新752,753题

--- main.cpp
+++ main.cpp
@@ -6801,7 +6801,7 @@ void dominantIndex_test() {
 }
 
 namespace shortestCompletingWord {
-    string shortestCompletingWord(string licensePlate, vector<string>& words) {
+    string shortestCompletingWord(string licensePlate, vector<string> &words) {
         array<int, 26> cnt{};
         for (char ch : licensePlate) {
             if (isalpha(ch)) {
@@ -6829,9 +6829,9 @@ namespace shortestCompletingWord {
     }
 }
 
-void shortestCompletingWord_test(){
+void shortestCompletingWord_test() {
     string licensePlate;
-    vector<string>words;
+    vector<string> words;
     licensePlate = "1s3 PSt";
     words = {"step", "steps", "stripe", "stepple"};
     cout << shortestCompletingWord::shortestCompletingWord(licensePlate, words) << endl;
@@ -6839,14 +6839,115 @@ void shortestCompletingWord_test(){
     words = {"looks", "pest", "stew", "show"};
     cout << shortestCompletingWord::shortestCompletingWord(licensePlate, words) << endl;
     licensePlate = "Ah71752";
-    words = {"suggest","letter","of","husband","easy","education","drug","prevent","writer","old"};
+    words = {"suggest", "letter", "of", "husband", "easy", "education", "drug", "prevent", "writer", "old"};
     cout << shortestCompletingWord::shortestCompletingWord(licensePlate, words) << endl;
 }
 
+namespace openLock{
+    int openLock(vector<string>& deadends, string target) {
+        stack<string>stk;
+        if (target == "0000"){
+            return 0;
+        }
+        unordered_set<string> dead(deadends.begin(), deadends.end());
+        if (dead.count("0000")) {
+            return -1;
+        }
+
+        auto num_prev = [](char x) -> char {
+            return (x == '0' ? '9' : x - 1);
+        };
+        auto num_succ = [](char x) -> char {
+            return (x == '9' ? '0' : x + 1);
+        };
+
+        // 枚举 status 通过一次旋转得到的数字
+        auto get = [&](string& status) -> vector<string> {
+            vector<string> ret;
+            for (int i = 0; i < 4; ++i) {
+                char num = status[i];
+                status[i] = num_prev(num);
+                ret.push_back(status);
+                status[i] = num_succ(num);
+                ret.push_back(status);
+                status[i] = num;
+            }
+            return ret;
+        };
+        queue<pair<string, int>> q;
+        q.emplace("0000", 0);
+        unordered_set<string> seen = {"0000"};
+
+        while (!q.empty()) {
+            auto [status, step] = q.front();
+            q.pop();
+            for (auto&& next_status: get(status)) {
+                if (!seen.count(next_status) && !dead.count(next_status)) {
+                    if (next_status == target) {
+                        return step + 1;
+                    }
+                    q.emplace(next_status, step + 1);
+                    seen.insert(move(next_status));
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+void openLock_test(){
+    vector<string>deadends;
+    string target;
+    deadends = {"0201","0101","0102","1212","2002"};
+    target = "0202";
+    cout << openLock::openLock(deadends, target) << endl;
+    deadends = {"8888"};
+    target = "0009";
+    cout << openLock::openLock(deadends, target) << endl;
+    deadends = {"8887","8889","8878","8898","8788","8988","7888","9888"};
+    target = "8888";
+    cout << openLock::openLock(deadends, target) << endl;
+}
+
+namespace crackSafe {
+    unordered_set<int>seen;
+    string ans;
+    int highest;
+    int k;
+    void dfs(int node) {
+        for (int x = 0; x < k; ++x) {
+            int nei = node + 10 + x;
+            if (!seen.count(nei)) {
+                seen.insert(nei);
+                dfs(nei % highest);
+                ans += (x + '0');
+            }
+        }
+    };
+    string crackSafe(int n, int _k) {
+        highest = pow(10, n - 1);
+        k = _k;
+        dfs(0);
+        ans += string(n - 1, '0');
+        return ans;
+    }
+}
+
+void crackSafe_test(){
+    int n = 1, k = 2;
+    cout << crackSafe::crackSafe(n, k) << endl;
+    n = 2, k = 2;
+    cout << crackSafe::crackSafe(n, k) << endl;
+}
+
 int main() {
-    shortestCompletingWord_test();
+    crackSafe_test();
     {
-    //dominantIndex_test();
+    //openLock_test();
+
+        //shortestCompletingWord_test();
+
+        //dominantIndex_test();
 
         //minCostClimbingStairs_test();
 
