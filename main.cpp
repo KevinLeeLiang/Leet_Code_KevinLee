51a6eea3eb587dc0966674a1385fac7c53fdd85a
garen-lee <srljl@163.com>
2024-10-09 18:16:03 +0800

    暂存

--- main.cpp
+++ main.cpp
@@ -7168,15 +7168,103 @@ void orderOfLargestPlusSign_test() {
     cout << orderOfLargestPlusSign::orderOfLargestPlusSign(n, mines) << endl;
     n = 1, mines = {{0, 0}};
     cout << orderOfLargestPlusSign::orderOfLargestPlusSign(n, mines) << endl;
-    n = 2, mines = {{0,0},{0,1},{1,0}};
+    n = 2, mines = {{0, 0},
+                    {0, 1},
+                    {1, 0}};
     cout << orderOfLargestPlusSign::orderOfLargestPlusSign(n, mines) << endl;
-    n = 3, mines = {{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+    n = 3, mines = {{0, 1},
+                    {0, 2},
+                    {1, 0},
+                    {1, 1},
+                    {1, 2},
+                    {2, 0},
+                    {2, 1},
+                    {2, 2}};
     cout << orderOfLargestPlusSign::orderOfLargestPlusSign(n, mines) << endl;
 }
 
+namespace minSwapsCouples {
+    int minSwapsCouples(vector<int> &row) {
+        int n = row.size();
+        int tot = n / 2;
+
+        vector<vector<int>> graph(tot);
+        for (int i = 0; i < n; i += 2) {
+            int l = row[i] / 2;
+            int r = row[i + 1] / 2;
+            if (l != r) {
+                graph[l].push_back(r);
+                graph[r].push_back(l);
+            }
+        }
+        vector<int> visited(tot, 0);
+        int ret = 0;
+        for (int i = 0; i < tot; i++) {
+            if (visited[i] == 0) {
+                queue<int> q;
+                visited[i] = 1;
+                q.push(i);
+                int cnt = 0;
+
+                while (!q.empty()) {
+                    int x = q.front();
+                    q.pop();
+                    cnt += 1;
+
+                    for (int y: graph[x]) {
+                        if (visited[y] == 0) {
+                            visited[y] = 1;
+                            q.push(y);
+                        }
+                    }
+                }
+                ret += cnt - 1;
+            }
+        }
+        return ret;
+    }
+}
+
+void minSwapsCouples_test() {
+    vector<int> row;
+    row = {0, 2, 1, 3};
+    cout << minSwapsCouples::minSwapsCouples(row) << endl;
+    row = {3, 2, 0, 1};
+    cout << minSwapsCouples::minSwapsCouples(row) << endl;
+}
+
+namespace isToeplitzMatrix {
+    bool isToeplitzMatrix(vector<vector<int>> &matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+void isToeplitzMatrix_test() {
+    vector<vector<int>> matrix;
+    matrix = {{1, 2, 3, 4},
+              {5, 1, 2, 3},
+              {9, 5, 1, 2}};
+    cout << isToeplitzMatrix::isToeplitzMatrix(matrix) << endl;
+    matrix = {{1, 2},
+              {2, 2}};
+    cout << isToeplitzMatrix::isToeplitzMatrix(matrix) << endl;
+}
+
 int main() {
-    orderOfLargestPlusSign_test();
+    isToeplitzMatrix_test();
     {
+        //minSwapsCouples_test();
+
+        //orderOfLargestPlusSign_test();
+
         //partitionLabels_test();
 
         //countPrimeSetBits_test();
