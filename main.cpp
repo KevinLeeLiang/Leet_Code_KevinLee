b2d6e029701d918ec94679addf449dda3cda59c0
garen_lee <lijialiang@haomo.ai>
2024-10-29 12:36:22 +0800

    更新815，816题

--- main.cpp
+++ main.cpp
@@ -8429,9 +8429,113 @@ void pruneTree_test(){
     cout << "-------------" << endl;
 }
 
+namespace numBusesToDestination {
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+        int n = routes.size();
+        vector<vector<int>> edge(n, vector<int>(n));
+        unordered_map<int, vector<int>> rec;
+        for (int i = 0; i < n; i++) {
+            for (int site : routes[i]) {
+                for (int j : rec[site]) {
+                    edge[i][j] = edge[j][i] = true;
+                }
+                rec[site].push_back(i);
+            }
+        }
+        vector<int>dis(n ,-1);
+        queue<int>que;
+        for (int bus: rec[source]) {
+            dis[bus] = 1;
+            que.push(bus);
+        }
+        while (!que.empty()) {
+            int x = que.front();
+            que.pop();
+            for (int y = 0; y < n; ++y) {
+                if (edge[x][y] && dis[y] == -1) {
+                    dis[y] = dis[x] + 1;
+                    que.push(y);
+                }
+            }
+        }
+        int ret = INT_MAX;
+        for (int bus : rec[target]) {
+            if (dis[bus] != -1) {
+                ret = min(ret, dis[bus]);
+            }
+        }
+        return ret == INT_MAX ? -1 : ret;
+    }
+}
+
+void numBusesToDestination_test(){
+    vector<vector<int>> routes;
+    int src, tar;
+    src = 1, tar = 6;
+    routes = {{1,2,7},{3,6,7}};
+    cout << numBusesToDestination::numBusesToDestination(routes, src, tar) << endl;
+    src = 15, tar = 12;
+    routes = {{7,12},{4,5,15},{6},{15,19},{9,12,13}};
+    cout << numBusesToDestination::numBusesToDestination(routes, src, tar) << endl;
+}
+
+namespace ambiguousCoordinates {
+    vector<string> getPos(string s) {
+        vector<string> pos;
+        if (s[0] != '0' || s == "0") pos.push_back(s);
+        for (int p = 1; p < s.size(); ++p) {
+            if ((p != 1 && s[0] == '0') || s.back() == '0') continue;
+            pos.push_back(s.substr(0, p) + "." + s.substr(p));
+        }
+        return pos;
+    }
+
+    vector<string> ambiguousCoordinates(string s) {
+        int n = s.size() - 2;
+        vector<string> res;
+        s = s.substr(1, s.size() - 2);
+        for (int l = 1; l < n; ++l) {
+            vector<string> lt = getPos(s.substr(0, l));
+            if (lt.empty()) continue;
+            vector<string> rt = getPos(s.substr(l));
+            if (rt.empty()) continue;
+            for (auto& i : lt) {
+                for (auto& j : rt) {
+                    res.push_back("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        return res;
+    }
+}
+
+void ambiguousCoordinates_test(){
+    string s;
+    vector<string>ans;
+    s = "(123)";
+    ans = ambiguousCoordinates::ambiguousCoordinates(s) ;
+    print_vector(ans);
+    s = "(00011)";
+    ans = ambiguousCoordinates::ambiguousCoordinates(s) ;
+    print_vector(ans);
+    s = "(0123)";
+    ans = ambiguousCoordinates::ambiguousCoordinates(s) ;
+    print_vector(ans);
+    s = "(100)";
+    ans = ambiguousCoordinates::ambiguousCoordinates(s) ;
+    print_vector(ans);
+}
+
 int main() {
-    pruneTree_test();
+    ambiguousCoordinates_test();
     {
+    //numBusesToDestination_test();
+
+    //pruneTree_test();
+
     //largestSumOfAverages_test();
 
         //largestTriangleArea_test();
