85b7deac6d0ee3616540a553bbf79cf8ccff36f5
garen_lee <srljl@163.com>
2025-01-15 18:33:45 +0800

    更新973，974题

--- main.cpp
+++ main.cpp
@@ -12153,9 +12153,91 @@ void isRationalEqual_test(){
     cout << isRationalEqual::isRationalEqual(s, t) << endl;
 }
 
+namespace kClosest {
+    mt19937 gen{random_device{}()};
+    void random_select(vector<vector<int>>& points, int left, int right, int k) {
+        int pivot_id = uniform_int_distribution<int>{left, right}(gen);
+        int pivot = points[pivot_id][0] * points[pivot_id][0] + points[pivot_id][1] * points[pivot_id][1];
+        swap(points[right], points[pivot_id]);
+        int i = left - 1;
+        for (int j = left; j < right; ++j) {
+            int dist = points[j][0] * points[j][0] + points[j][1] * points[j][1];
+            if (dist <= pivot) {
+                ++i;
+                swap(points[i], points[j]);
+            }
+        }
+        ++i;
+        swap(points[i], points[right]);
+        // [left, i-1] 都小于等于 pivot, [i+1, right] 都大于 pivot
+        if (k < i - left + 1) {
+            random_select(points, left, i - 1, k);
+        }
+        else if (k > i - left + 1) {
+            random_select(points, i + 1, right, k - (i - left + 1));
+        }
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        random_select(points, 0, n - 1, k);
+        return {points.begin(), points.begin() + k};
+    }
+}
+
+void kClosest_test(){
+    vector<vector<int>> points, ans;
+    int k;
+    points = {{1,3},{-2,2}};
+    k = 1;
+    ans = kClosest::kClosest(points, k);
+    for (auto p : ans) {
+        cout << p[0] << "," << p[1]<<";";
+    }
+    cout << endl;
+    cout << "++++++++++++++++" << endl;
+    points = {{3,3},{5,-1},{-2,4}};
+    k = 2;
+    ans = kClosest::kClosest(points, k);
+    for (auto p : ans) {
+        cout << p[0] << "," << p[1]<<";";
+    }
+    cout << endl;
+    cout << "++++++++++++++++" << endl;
+}
+
+namespace subarraysDivByK {
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> record = {{0,1}};
+        int sum = 0, ans = 0;
+        for (int elem : nums) {
+            sum += elem;
+            int modules = (sum % k + k) % k;
+            if (record.count(modules)) {
+                ans += record[modules];
+            }
+            ++record[modules];
+        }
+        return ans;
+    }
+}
+
+void subarraysDivByK_test() {
+    vector<int>nums;
+    int k;
+    nums = {4,5,0,-2,-3,1}, k = 5;
+    cout << subarraysDivByK::subarraysDivByK(nums, k) << endl;
+    nums = {5}, k = 9;
+    cout << subarraysDivByK::subarraysDivByK(nums, k) << endl;
+}
+
 int main() {
-    isRationalEqual_test();
+    subarraysDivByK_test();
     {
+        //kClosest_test();
+
+        //isRationalEqual_test();
+
         //flipMatchVoyage_test();
 
         //powerfulIntegers_test();
